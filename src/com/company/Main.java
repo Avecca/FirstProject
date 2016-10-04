687fc83ef35b476dc6b73319b43e0676188c6ed9
Hanna Astlind <hanna_astlind@hotmail.com>
2016-10-04 16:49:25 +0200

    Commit 6- Revert

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("First try:");
+
         System.out.println("Hello World!");
-
-        //comment comment
-
-        System.out.println("Yay git works!");
-        System.out.println("Yay git works!");
     }
 }
